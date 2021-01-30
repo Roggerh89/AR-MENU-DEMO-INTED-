d4a3c7be2d389f302b481f16031b67897fdf3741
Roggerh89 <77993114+Roggerh89@users.noreply.github.com>
2021-01-30 11:25:05 -0500

    Rogger 30/01/2020

---
+++ AE_Menu Project/Assets/Canvas/Scripts/AbrirWhatsapp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirWhatsapp : MonoBehaviour
+{
+    public void AbrirWP()
+    {
+        Application.OpenURL("https://api.whatsapp.com/send?phone=51986146761&text=hola!");
+    }
+
+
+}
