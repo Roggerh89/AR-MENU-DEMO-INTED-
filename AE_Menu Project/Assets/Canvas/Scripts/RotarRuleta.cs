577212a28834a40c0963ab6d967f87b89c98137e
Roggerh89 <77993114+Roggerh89@users.noreply.github.com>
2021-02-25 11:31:29 -0500

    25/02/21 - Rogger

---
+++ AE_Menu Project/Assets/Canvas/Scripts/RotarRuleta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarRuleta : MonoBehaviour {
+    public float rotationSpeed = 0.1f;
+    private  Vector3 lastPos;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            lastPos = (Input.mousePosition);
+        }
+          else if (Input.GetMouseButton(0)) {
+            var rotationY = ((Input.mousePosition).y - lastPos.y) * rotationSpeed;
+            transform.Rotate(Vector3.right, -rotationY, Space.Self);
+           lastPos = Input.mousePosition;
+        }
+    }
+}
+
---
+++ AE_Menu Project/Assets/CircleLoopScrollRect/Assets/Scripts/CircleTestItem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* 
+    Dos Cambio reaalidos:
+    1. el numero de la lista siempre regrese 8 
+    2. borras nameTxt.text y IconImg.sprite -> ya que si no lo haces aparecera el numero indexado en texto  y cambiara el source image por diferentes botones 
+    IMPORTANTE: Cambiar el length de la list al requerido.
+    */
+
+public class CircleItemContent
+{
+    public string name;
+    public Sprite icon;
+}
+//Overide
+public class CircleTestItem : CircleScrollRectItemBase
+{
+    public Text nameTxt;
+    public Image iconImg;
+
+    //Override following three functions 
+    public override void RefreshItemContent(object data)
+    {
+        var contentItem = data as CircleItemContent;
+     //   if(null != contentItem)
+     //   {
+     //       if(null != nameTxt)
+     //       {
+     //           nameTxt.text = contentItem.name;
+     //       }
+     //       if(null != iconImg)
+     //       {
+     //           iconImg.sprite = contentItem.icon;
+    //        }          
+    //    }
+        
+    }
+
+
+
+    public override object GetContentListItem(int index)
+    {
+        return 0;
+        //  return TestMono.ins.GetItem(index);
+    }
+
+    public override int GetContentListLength()
+    {
+        return 8;
+     //   return TestMono.ins.GetItemLength();
+    }
+
+}
